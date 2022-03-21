314e623f40f998413a93ff89767d28756289ebac
yousufalaa10 <yousufalaa10@yahoo.com>
2022-03-21 21:09:53 +0200

    Navbar

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -13,11 +13,19 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="<?php echo URLROOT; ?>css/style.css">
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>css/navbar.css">
+
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>fonts/icomoon/style.css">
+    <script src="<?php echo URLROOT; ?>js/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo URLROOT; ?>js/popper.min.js"></script>
+    <script src="<?php echo URLROOT; ?>js/bootstrap.min.js"></script>
+    <script src="<?php echo URLROOT; ?>js/jquery.sticky.js"></script>
+    <script src="<?php echo URLROOT; ?>js/main.js"></script>
  
+
   <title><?php echo SITENAME; ?></title>
 </head>
 
 <body>
   <?php require APPROOT . '/views/inc/navbar.php'; ?>
-  <div class="container">
+  <!-- <div class="container"> -->
