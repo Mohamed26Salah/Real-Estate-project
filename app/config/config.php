97b4674f4ae1743b6c12ab20dbe959750c8751ce
Mohamed Salah <mohamedsalahomran44@gmail.com>
2022-06-01 22:52:32 +0200

    Deploy

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // DB Params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'newuser');
+define('DB_HOST', '104.248.26.63');
+define('DB_USER', 'root');
 define('DB_PASS', '54610a90981e81e3f4fe62a12b30d0be05c5f3408f6b1589');
 define('DB_NAME', 'real-estate project');
 
@@ -9,7 +9,7 @@ define('DB_NAME', 'real-estate project');
 //echo dirname(dirname(__FILE__));
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/mvc/public/');
+define('URLROOT', 'http://104.248.26.63/mvc/public/');
 // Site Name
 define('SITENAME', 'MataNa');
 //App Version
@@ -19,6 +19,6 @@ define('VIEWS_PATH', '../app/views/');
 define('models_PATH', '../app/models/');
 //Imagese
 define('IMAGEROOT','../app/views/images/');
-define('IMAGEROOT2','http://localhost/mvc/public/images/');
-define('IMAGEROOT3','http://localhost/mvc/app/views/images/');
+define('IMAGEROOT2','http://104.248.26.63/mvc/public/images/');
+define('IMAGEROOT3','http://104.248.26.63/mvc/app/views/images/');
 
