c104b775fc76220ad60000420abfedc618181dc7
Mohamed Salah <mohamedsalahomran44@gmail.com>
2022-06-01 22:43:50 +0200

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // DB Params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+define('DB_USER', 'newuser');
 define('DB_PASS', '54610a90981e81e3f4fe62a12b30d0be05c5f3408f6b1589');
 define('DB_NAME', 'real-estate project');
 
