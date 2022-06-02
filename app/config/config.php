f2203e6b2e2f81f2574f0a215ade5a43a0221465
Mohamed Salah <mohamedsalahomran44@gmail.com>
2022-06-02 12:44:42 +0200

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -3,10 +3,10 @@
 //firstDroplet Database
 define('DB_HOST', '104.248.26.63');
 
-define('DB_USER', 'newuser');
+define('DB_USER', '');
 //firstDroplet Database
-// define('DB_PASS', '54610a90981e81e3f4fe62a12b30d0be05c5f3408f6b1589');
-define('DB_PASS', 'Convertmybaby2');
+define('DB_PASS', '54610a90981e81e3f4fe62a12b30d0be05c5f3408f6b1589');
+
 define('DB_NAME', 'real-estate project');
 
 // App Root
